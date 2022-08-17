8881d7b41d317d6ecb5a13124940bc381078a5c4
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-08-17 14:51:30 -0500

    Validación de entrada

--- js/criptex.js
+++ js/criptex.js
@@ -1,24 +1,33 @@
 const btnencriptar = document.getElementById('btnencriptar');
 const btndesencriptar = document.getElementById('btndesencriptar');
-
-
+const divsaludo = document.getElementById("divsaludo");
+const divsalida = document.getElementById("divsalida");
+const taentrada = document.getElementById("taentrada");
+const tasalida = document.getElementById("tasalida");
 
 btnencriptar.addEventListener('click', function handleClick() {
-    alert("Hola! Encriptar");
-    muestraResultado();
+    let texto = taentrada.value;
+    var patt = new RegExp(/^[a-z\s]+$/g);
+    if (patt.test(texto)) {
+        tasalida.value = "encriptado:" + texto;
+        muestraResultado();
+    } else
+        alert("Por favor ingresa solo letras minúsculas y sin acentos");
 });
 
 btndesencriptar.addEventListener('click', function handleClick() {
-    alert("Hola! Desencriptar");
-    muestraResultado();
+    let texto = taentrada.value;
+    var patt = new RegExp(/^[a-z\s]+$/g);
+    if (patt.test(texto)) {
+        tasalida.value = "desencriptado:" + texto;
+        muestraResultado();
+    } else
+        alert("Por favor ingresa solo letras minúsculas y sin acentos");
 });
 
 
 
 function muestraResultado() {
-    const divsaludo = document.getElementById("divsaludo");
-    const divsalida = document.getElementById("divsalida");
-
     divsaludo.style.display = "none";
     divsalida.style.display = "block";
 }
