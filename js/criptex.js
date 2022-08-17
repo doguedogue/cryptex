2bbff3f179ecd54407c49ca322b1a566d70facd1
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-08-17 15:04:53 -0500

    Copy to Clipboard

--- js/criptex.js
+++ js/criptex.js
@@ -1,13 +1,18 @@
 const btnencriptar = document.getElementById('btnencriptar');
 const btndesencriptar = document.getElementById('btndesencriptar');
+const btncopiar = document.getElementById('btncopiar');
 const divsaludo = document.getElementById("divsaludo");
 const divsalida = document.getElementById("divsalida");
 const taentrada = document.getElementById("taentrada");
 const tasalida = document.getElementById("tasalida");
 
 btnencriptar.addEventListener('click', function handleClick() {
-    let texto = taentrada.value;
+    let texto = taentrada.value.trim();
     var patt = new RegExp(/^[a-z\s]+$/g);
+    if (texto.length == 0) {
+        return;
+    }
+
     if (patt.test(texto)) {
         tasalida.value = "encriptado:" + texto;
         muestraResultado();
@@ -16,7 +21,11 @@ btnencriptar.addEventListener('click', function handleClick() {
 });
 
 btndesencriptar.addEventListener('click', function handleClick() {
-    let texto = taentrada.value;
+    let texto = taentrada.value.trim();
+    if (texto.length == 0) {
+        return;
+    }
+
     var patt = new RegExp(/^[a-z\s]+$/g);
     if (patt.test(texto)) {
         tasalida.value = "desencriptado:" + texto;
@@ -25,7 +34,17 @@ btndesencriptar.addEventListener('click', function handleClick() {
         alert("Por favor ingresa solo letras minúsculas y sin acentos");
 });
 
+btncopiar.addEventListener('click', function handleClick() {
+    copyToClipboard();
+});
 
+
+const copyToClipboard = () => {
+    tasalida.select();
+    tasalida.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    tasalida.setSelectionRange(0, 0);
+};
 
 function muestraResultado() {
     divsaludo.style.display = "none";
